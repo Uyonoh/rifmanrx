5ad8b1383cc5ed37eddc05b52aca728f7dde3d3f
Uyonoh <turakiuyonoh@gmail.com>
2024-07-16 14:30:31 +0100

    create superuser

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 app_name = "users"
 
 urlpatterns = [
-    path("login", include("django.contrib.auth.urls")),
+    path("login/", include("django.contrib.auth.urls")),
 ]

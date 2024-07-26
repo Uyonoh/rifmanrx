1d002f9ff4215c7bc183e840c67275addce13ff6
Uyonoh <turakiuyonoh@gmail.com>
2024-07-26 20:51:50 +0100

    add purchase form

--- books/forms.py
+++ books/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Sale
+from .models import Sale, Purchase
 
 
 class SaleForm(forms.ModelForm):
@@ -17,4 +17,22 @@ class SaleForm(forms.ModelForm):
 
     class Meta:
         model = Sale
+        exclude = ["time"]
+
+
+class PurchaseForm(forms.ModelForm):
+    """ Purchase form """
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def set_classes(self, drug):
+        """ Set form classes """
+
+        self.fields["drug"].widget.attrs["pk"] = f"{drug.pk}"
+
+    
+
+    class Meta:
+        model = Purchase
         exclude = ["time"]

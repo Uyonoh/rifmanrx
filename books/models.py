6f6ef65005a41ad6c964ca99963dee439a9be716
Uyonoh <turakiuyonoh@gmail.com>
2024-07-26 20:48:43 +0100

    add credit, debit, and purchase models in books

--- books/models.py
+++ books/models.py
@@ -4,6 +4,28 @@ from drugs.models import Drug
 
 # Create your models here.
 
+class Credit(models.Model):
+    """ Records of all crediting transactions """
+
+    item = models.CharField(max_length=100)
+    amount = models.FloatField()
+    date = models.DateField(default=tz.now().date)
+
+    def __str__(self) -> str:
+        return f"Credit for {self.item}"
+
+
+class Debit(models.Model):
+    """ Records of all debiting transactions """
+
+    item = models.CharField(max_length=100)
+    amount = models.FloatField()
+    date = models.DateField(default=tz.now().date)
+
+    def __str__(self) -> str:
+        return f"Debit for {self.item}"
+
+
 class Sale(models.Model):
     drug = models.ForeignKey(Drug, on_delete=models.CASCADE)
     amount = models.IntegerField()
@@ -17,3 +39,12 @@ class Sale(models.Model):
     
     def __str__(self) -> str:
         return f"Sale of {self.amount} {self.drug.name}(s) on {self.time.date()} at {self.time.time()}"
+    
+
+class Purchase(models.MOdel):
+    drug = models.ForeignKey(Drug, on_delete=models.CASCADE)
+    price = models.FloatField()
+    date = models.DateField(default=tz.now().date)
+
+    def __str__(self) -> str:
+         return f"Purchase of {self.amount} {self.drug.name}(s) on {self.date}"
